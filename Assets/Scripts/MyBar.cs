d1874c6793fa3debe92591117dd77eeeab684e04
seobyeongky <seobyeongky@naver.com>
2015-11-21 14:59:27 +0900

    hard dev

---
+++ Assets/Scripts/MyBar.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+[ExecuteInEditMode]
+public class MyBar : MonoBehaviour
+{
+    // 0 ~ 100
+    public Observable<float> progress = new Observable<float>(0);
+    public Image content;
+    public Text leftText;
+    public Text rightText;
+
+	void Awake ()
+    {
+        progress.Listen(UpdateText);
+        progress.Value = content.fillAmount * 100f;
+    }
+
+#if UNITY_EDITOR
+    void Update()
+    {
+        progress.Value = content.fillAmount * 100f;
+    }
+#endif
+
+    void UpdateText(float newValue)
+    {
+        if (newValue > 50f)
+        {
+            leftText.gameObject.SetActive(true);
+            rightText.gameObject.SetActive(false);
+            leftText.text = newValue.ToString();
+        }
+        else
+        {
+            leftText.gameObject.SetActive(false);
+            rightText.gameObject.SetActive(true);
+            rightText.text = newValue.ToString();
+        }
+    }
+}
---
+++ Assets/Scripts/Observable.cs
@@ -0,0 +1,122 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class Observable<T>
+{
+#if UNITY_EDITOR
+    public bool dbg = false;
+#endif
+
+    private T obj;
+    private int idCount = 0;
+    private Dictionary<int, System.Action<T>> dict = new Dictionary<int, System.Action<T>>();
+    private List<System.Action<T>> disposables = new List<System.Action<T>>();
+    private bool isDictIterating = false;
+    private List<int> deathIds = new List<int>();
+
+    public Observable()
+    {
+    }
+
+    public Observable(T initObj)
+    {
+        obj = initObj;
+#if UNITY_EDITOR
+        if (dbg)
+        {
+            Debug.Log("observable init with " + initObj);
+        }
+#endif
+    }
+
+
+    public T Value
+    {
+        get
+        {
+#if UNITY_EDITOR
+            if (dbg)
+            {
+                Debug.Log("observable get " + obj);
+            }
+#endif
+            return obj;
+        }
+
+        set
+        {
+            obj = value;
+
+#if UNITY_EDITOR
+            if (dbg)
+            {
+                Debug.Log("observable set " + obj);
+            }
+#endif
+
+            isDictIterating = true;
+            foreach (var item in dict)
+            {
+                item.Value(obj);
+            }
+            isDictIterating = false;
+
+            if (deathIds.Count > 0)
+            {
+                foreach (int id in deathIds)
+                {
+                    dict.Remove(id);
+                }
+                deathIds.Clear();
+            }
+
+            foreach (var callback in disposables)
+            {
+                callback(obj);
+            }
+            disposables.Clear();
+        }
+    }
+
+    public int Listen(System.Action<T> callback)
+    {
+        int id = idCount++;
+        dict.Add(id, callback);
+        return id;
+    }
+
+    public void ListenOnce(System.Action<T> callback)
+    {
+        disposables.Add(callback);
+    }
+
+    public void StopListen(int id)
+    {
+        if (dict.ContainsKey(id) == false)
+        {
+            throw new System.Exception("no such id : " + id);
+        }
+
+        if (isDictIterating)
+        {
+            deathIds.Add(id);
+        }
+        else
+        {
+            dict.Remove(id);
+        }
+    }
+
+    public delegate T COMP1<K>(K obj);
+    public static Observable<T> Compute<K>(Observable<K> a, COMP1<K> func)
+    {
+        Observable<T> ret = new Observable<T>();
+
+        a.Listen(x =>
+        {
+            ret.Value = func(x);
+        });
+
+        return ret;
+    }
+}
