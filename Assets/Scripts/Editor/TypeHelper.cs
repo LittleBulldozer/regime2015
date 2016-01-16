a45ed90555146e5f53177491437345801fdfd173
seobyeongky <seobyeongky@naver.com>
2016-01-16 14:53:41 +0900

    build ok

---
+++ Assets/Scripts/Editor/TypeHelper.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using System.Reflection;
+
+public class TypeHelper : MonoBehaviour
+{
+    public static System.Type[] GetAllSubTypes(System.Type ParentType)
+    {
+        List<System.Type> list = new List<System.Type>();
+        foreach (Assembly a in System.AppDomain.CurrentDomain.GetAssemblies())
+        {
+            foreach (System.Type type in a.GetTypes())
+            {
+                if (type.IsSubclassOf(ParentType))
+                {
+                    list.Add(type);
+                }
+            }
+        }
+        return list.ToArray();
+    }
+}
