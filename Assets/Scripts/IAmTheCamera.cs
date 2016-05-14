0f0996aab886ba95e1463ed5f3f0ba28fa467e08
seobyeongky <seobyeongky@naver.com>
2016-05-14 21:36:44 +0900

    렌즈 효과 개이득

---
+++ Assets/Scripts/IAmTheCamera.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class IAmTheCamera : MonoBehaviour
+{
+    public Camera cam;
+
+    void OnEnable()
+    {
+        Reposition();
+
+        ScreenExtension.OnSizeChange1 += OnScreenSizeChange;
+    }
+
+    void OnDisable()
+    {
+        ScreenExtension.OnSizeChange1 -= OnScreenSizeChange;
+    }
+
+    void Reposition()
+    {
+        Debug.Log("Reposition!");
+
+        cam.transform.position = transform.position - 1 * transform.forward;
+        cam.transform.LookAt(transform);
+        cam.orthographic = true;
+
+        var size = transform.GetComponent<BoxCollider>().size;
+        var LS = transform.localScale;
+        size.x *= LS.x;
+        size.y *= LS.y;
+        cam.orthographicSize = 0.5f * size.y;
+    }
+
+    void OnScreenSizeChange(int width, int height)
+    {
+        Reposition();
+    }
+}
---
+++ Assets/Scripts/PlaneRenderer.cs
@@ -0,0 +1,94 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class PlaneRenderer : MonoBehaviour
+{
+    public Renderer plane;
+
+    RenderTexture RT = null;
+    Camera cam = null;
+
+    void Awake()
+    {
+        cam = GetComponent<Camera>();
+    }
+
+    void OnEnable()
+    {
+        MakeRenderTarget();
+        ScreenExtension.OnSizeChange0 += OnScreenSizeChange;
+    }
+
+    void OnDisable()
+    {
+        ScreenExtension.OnSizeChange0 -= OnScreenSizeChange;
+        ReleaseRenderTarget();
+    }
+
+    void MakeRenderTarget()
+    {
+        Debug.Log("[PlaneRenderer] MakeRenderTarget");
+
+        RT = new RenderTexture(Screen.width, Screen.height, 24, RenderTextureFormat.Default);
+        cam.targetTexture = RT;
+        if (plane != null)
+        {
+            (
+                Application.isPlaying
+                ? plane.material
+                : plane.sharedMaterial
+            )
+            .SetTexture("_MainTex", RT);
+
+            var LS = plane.transform.localScale;
+            LS.y = LS.x * Screen.height / Screen.width;
+            plane.transform.localScale = LS;
+        }
+    }
+
+    void ReleaseRenderTarget()
+    {
+        cam.targetTexture = null;
+        RT.Release();
+        RT = null;
+    }
+
+    void OnScreenSizeChange(int width, int height)
+    {
+        ReleaseRenderTarget();
+        MakeRenderTarget();
+    }
+
+#if UNITY_EDITOR
+    void Update()
+    {
+        if (Application.isPlaying == false)
+        {
+            if (RT == null)
+            {
+                MakeRenderTarget();
+            }
+
+            if (cam.targetTexture != RT)
+            {
+                cam.targetTexture = RT;
+            }
+
+            if (plane != null)
+            {
+                var mat = (
+                    Application.isPlaying
+                    ? plane.material
+                    : plane.sharedMaterial
+                );
+
+                if (mat.GetTexture("_MainTex") != RT)
+                {
+                    mat.SetTexture("_MainTex", RT);
+                }
+            }
+        }
+    }
+#endif
+}
